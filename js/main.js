27aaaea421fce51b4a89d752caf0ed1c60d31b6b
Kevin <vuibinhyen2003@gmail.com>
2022-06-16 15:29:33 +0700

    Almostly finished

--- js/main.js
+++ js/main.js
@@ -15,15 +15,40 @@
 // callAPI().then(data=>{
 //     console.log(data)
 // })
-// document.querySelector(".js-showroom").onclick = () => {
-//        document.querySelector("#_showroom").classList.toggle("slideIn")
-// }
+const SHOWROOM = document.querySelector("#_showroom")
+const OPEN_SHOWROOM = document.querySelector(".js-showroom")
+const CLOSE_SHOWROOM = document.querySelector(".showroom-close")
+OPEN_SHOWROOM.onclick = () => {
+    SHOWROOM.classList.add("slideIn")
+    document.querySelector(".js-arrow").classList.replace("fa-angle-down", "fa-angle-up")
 
-// if (window.innerWidth >= 768) {
-//        document.querySelectorAll(".link-list").forEach(item => item.classList.add("collapse"))
+}
+CLOSE_SHOWROOM.onclick= (e)=>{
+    e.stopPropagation()
+    SHOWROOM.classList.remove("slideIn")
+    document.querySelector(".js-arrow").classList.replace("fa-angle-up", "fa-angle-down")
+}
+const PLUS_ICON = document.querySelectorAll(".js-plus")
+const LINK_HEADING = document.querySelectorAll(".js-heading")
+LINK_HEADING.forEach((item, index) => item.onclick = () => {
+    if (PLUS_ICON[index].classList.contains("fa-plus"))
+        PLUS_ICON[index].classList.replace("fa-plus", "fa-minus")
+    else {
+        PLUS_ICON[index].classList.replace("fa-minus", "fa-plus")
 
-// } else {
-//        document.querySelectorAll(".link-list").forEach(item => item.classList.remove("collapse"))
+    }
+}
 
-//        // document.querySelectorAll(".link-list").classList.remove("collapse")
-// }
+)
+
+
+const NAVBAR_TOGGLER = document.querySelector(".navbar-toggler")
+const SHOW_MENU = document.querySelectorAll(".navbar-toggler-icon")
+const MENU_TEXT = document.querySelector(".menu-text")
+console.log(SHOW_MENU)
+console.log(NAVBAR_TOGGLER)
+NAVBAR_TOGGLER.onclick =()=>{
+    SHOW_MENU[0].classList.toggle("transform45deg")
+    SHOW_MENU[1].classList.toggle("transform-45deg")
+    MENU_TEXT.classList.toggle("fadeOut")
+}
