266e57c1a361e1916315c7ba433db265e62df62a
Kevin <vuibinhyen2003@gmail.com>
2022-06-21 14:00:51 +0700

    final

--- js/main.js
+++ js/main.js
@@ -1,35 +1,40 @@
-// async function  callAPI(){
-//     try{
-//         let res = await axios(
-//             {
-//                 // url: "http://35.154.4.154/mobile_ae/app/v0_1/api/home_page" , 
-//                 method : "GET"
-//             }
-//         )
-
-//         return res.data
-//     }catch(err){
-//         console.log(err)
-//     }
-// }
-// callAPI().then(data=>{
-//     console.log(data)
-// })
 const SHOWROOM = document.querySelector("#_showroom")
+const ICON_ARROW = document.querySelectorAll(".js-arrow")
 const OPEN_SHOWROOM = document.querySelector(".js-showroom")
 const CLOSE_SHOWROOM = document.querySelector(".showroom-close")
-OPEN_SHOWROOM.onclick = () => {
-    SHOWROOM.classList.add("slideIn")
-    document.querySelector(".js-arrow").classList.replace("fa-angle-down", "fa-angle-up")
+const OPEN_DROPDOWN = document.querySelectorAll(".js-dropdown")
+const DROPDOWN_LIST = document.querySelectorAll(".dropdown")
+const PLUS_ICON = document.querySelectorAll(".js-plus")
+const LINK_HEADING = document.querySelectorAll(".js-heading")
+const NAVBAR_TOGGLER = document.querySelector(".navbar-toggler")
+const SHOW_MENU = document.querySelectorAll(".navbar-toggler-icon")
+const MENU_TEXT = document.querySelector(".menu-text")
 
+
+function slideFromTop(selector, _class, _icon) {
+    selector.classList.toggle(_class)
+    if (selector.classList.contains(_class)) {
+        _icon.classList.replace("fa-angle-down", "fa-angle-up")
+    } else {
+        _icon.classList.replace("fa-angle-up", "fa-angle-down")
+
+    }
 }
-CLOSE_SHOWROOM.onclick= (e)=>{
+
+OPEN_SHOWROOM.onclick = () => slideFromTop(SHOWROOM, "slideIn", ICON_ARROW[0])
+CLOSE_SHOWROOM.onclick = (e) => {
     e.stopPropagation()
     SHOWROOM.classList.remove("slideIn")
     document.querySelector(".js-arrow").classList.replace("fa-angle-up", "fa-angle-down")
 }
-const PLUS_ICON = document.querySelectorAll(".js-plus")
-const LINK_HEADING = document.querySelectorAll(".js-heading")
+
+
+
+OPEN_DROPDOWN.forEach((item, index) => {
+    item.onclick = () => slideFromTop(DROPDOWN_LIST[index], "slide", ICON_ARROW[index + 1])
+})
+// Plus <=> Minus
+
 LINK_HEADING.forEach((item, index) => item.onclick = () => {
     if (PLUS_ICON[index].classList.contains("fa-plus"))
         PLUS_ICON[index].classList.replace("fa-plus", "fa-minus")
@@ -41,20 +46,16 @@ LINK_HEADING.forEach((item, index) => item.onclick = () => {
 
 )
 
+// Menu navbar
 
-const NAVBAR_TOGGLER = document.querySelector(".navbar-toggler")
-const SHOW_MENU = document.querySelectorAll(".navbar-toggler-icon")
-const MENU_TEXT = document.querySelector(".menu-text")
-console.log(SHOW_MENU)
-console.log(NAVBAR_TOGGLER)
-NAVBAR_TOGGLER.onclick =()=>{
-   
+NAVBAR_TOGGLER.onclick = () => {
+
     SHOW_MENU[0].classList.toggle("transform45deg")
     SHOW_MENU[1].classList.toggle("transform-45deg")
     MENU_TEXT.classList.toggle("fadeOut")
-    if(SHOWROOM.classList.contains("slideIn")){
+    if (SHOWROOM.classList.contains("slideIn")) {
         SHOWROOM.classList.remove("slideIn")
-    document.querySelector(".js-arrow").classList.replace("fa-angle-up", "fa-angle-down")
+        document.querySelector(".js-arrow").classList.replace("fa-angle-up", "fa-angle-down")
 
     }
 }
